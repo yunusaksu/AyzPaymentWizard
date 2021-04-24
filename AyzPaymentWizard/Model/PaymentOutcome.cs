e550c12bb6f1b6c2affefa3e19680d3c77ffa5b1
yunusaksu <dev.yunus.aksu@gmail.com>
2021-04-24 03:22:28 +0300

    FileHelpers class'ı içindeki PAYMENTOUTCOME, RecordDetails ve FOOTER class ları PaymentOutcome.cs altına taşındı

--- AyzPaymentWizard/Model/PaymentOutcome.cs
+++ AyzPaymentWizard/Model/PaymentOutcome.cs
@@ -8,6 +8,73 @@ using System.Threading.Tasks;
 namespace AyzPaymentWizard.Model
 {
     [FixedLengthRecord(FixedMode.ExactLength)]
+    public class RecordDetails
+    {
+        [FieldFixedLength(1)]
+        public string Dstring { get; set; }
+        [FieldFixedLength(4)]
+
+        [FieldAlign(AlignMode.Right, '0')]
+        public string HEDEFBANKA { get; set; }
+
+        [FieldFixedLength(5)]
+        [FieldAlign(AlignMode.Right, '0')]
+        public string HEDEFSUBE { get; set; }
+
+        [FieldFixedLength(19)]
+        [FieldAlign(AlignMode.Left, ' ')]
+        public string HEDEFHESAPNUMARASI { get; set; }
+
+        [FieldFixedLength(3)]
+        public string PARAKODU { get; set; }
+        [FieldFixedLength(20)]
+
+        [FieldAlign(AlignMode.Right, '0')]
+        public decimal TUTAR { get; set; }
+
+        [FieldFixedLength(60)]
+        [FieldAlign(AlignMode.Left, ' ')]
+        public string ACIKLAMA { get; set; }
+
+        [FieldFixedLength(20)]
+        [FieldAlign(AlignMode.Left, ' ')]
+        public string FIRMAREFERANS { get; set; }
+
+        [FieldFixedLength(50)]
+        [FieldAlign(AlignMode.Left, ' ')]
+        public string UNVAN { get; set; }
+
+        [FieldFixedLength(50)]
+        [FieldAlign(AlignMode.Left, ' ')]
+        public string ADDRESS { get; set; }
+
+        [FieldFixedLength(10)]
+
+        [FieldAlign(AlignMode.Right, ' ')]
+        public string TELEFON { get; set; }
+
+        [FieldFixedLength(10)]
+        [FieldAlign(AlignMode.Left, ' ')]
+        public string VERGIKIMLIKNO { get; set; }
+
+        [FieldFixedLength(30)]
+        [FieldAlign(AlignMode.Left, ' ')]
+        public string VERGIDAIRESI { get; set; }
+
+        [FieldFixedLength(150)]
+        [FieldAlign(AlignMode.Left, ' ')]
+        public string EMAIL { get; set; }
+
+        [FieldFixedLength(26)]
+        [FieldAlign(AlignMode.Left, ' ')]
+        public string IBAN { get; set; }
+
+        [FieldFixedLength(16)]
+        [FieldAlign(AlignMode.Left, ' ')]
+        public string TCKN { get; set; }
+    }
+
+    [FixedLengthRecord(FixedMode.ExactLength)]
     [IgnoreEmptyLines]
     [IgnoreFirst(1)]
     [IgnoreLast(1)]
@@ -61,5 +128,20 @@ namespace AyzPaymentWizard.Model
 
         [FieldFixedLength(26)]
         public string IBAN;
+    }
+
+    [FixedLengthRecord(FixedMode.ExactLength)]
+    public class FOOTER
+    {
+        [FieldFixedLength(1)]
+        public string PAYMENT_TYPE { get; set; }
+
+        [FieldFixedLength(12)]
+        [FieldAlign(AlignMode.Right, '0')]
+        public string PAYMENT_COUNT { get; set; }
+
+        [FieldFixedLength(20)]
+        [FieldAlign(AlignMode.Right, '0')]
+        public string PAYMENT_TOTAL { get; set; }
     }
 }
