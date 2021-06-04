bc1274640646cc833cf86462c2137ec1eee2c355
Shirazbello <shirazbello@gmail.com>
2021-06-04 14:29:18 +0200

    Boyut Filterleme Textlere Arama Buttonlari eklendi

--- AyzPaymentWizard/Properties/Resources.Designer.cs
+++ AyzPaymentWizard/Properties/Resources.Designer.cs
@@ -73,6 +73,16 @@ namespace AyzPaymentWizard.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap Search {
+            get {
+                object obj = ResourceManager.GetObject("Search", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap show_icon {
             get {
                 object obj = ResourceManager.GetObject("show_icon", resourceCulture);
